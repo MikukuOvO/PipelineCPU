0b36c022155e2a25cd809172bd08328ac4353b90
MikukuOvO <mikukuovo@gmail.com>
2023-08-11 21:41:14 +0800

    QwQ

--- dm_controller.v
+++ dm_controller.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2023/06/26 15:26:10
-// Design Name: 
-// Module Name: dm_controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 `include "ctrl_encode_def.v"
 module dm_controller(
     input mem_w,
