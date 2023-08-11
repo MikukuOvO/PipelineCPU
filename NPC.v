38fe578f204eed04907373d032a3fb1d2839c311
MikukuOvO <mikukuovo@gmail.com>
2023-08-11 21:55:44 +0800

    Modify PC

--- NPC.v
+++ NPC.v
@@ -1,14 +1,17 @@
-module NPC(                  // next pc module
-    input  [31:0] PC,        // pc
-    input  [2:0]  NPCOp,     // next pc operation
-    input  [31:0] IMM,       // immediate
-    input  [31:0] aluout,
-    output reg [31:0] NPC    // next pc
+/*
+    This module implements the NPC unit
+*/
+module NPC(
+    input [31:0] PC,
+    input [2:0] NPCOp,
+    input [31:0] IMM,
+    input [31:0] aluout,
+    output reg [31:0] NPC
 );
     always @(*) begin
         case (NPCOp)
-            `NPC_BRANCH: NPC = PC+IMM;
-            `NPC_JUMP:   NPC = PC+IMM;
+            `NPC_BRANCH: NPC = PC + IMM;
+            `NPC_JUMP:   NPC = PC + IMM;
             `NPC_JALR:   NPC = aluout;
         endcase
     end
--- PC.v
+++ PC.v
@@ -1,18 +1,21 @@
+/*
+    This module implements the PC unit
+*/
 module PC(
-    input              clk,
-    input              rst,
-    input              stall,
-    input       [2:0]  NPCOP,
-    input       [31:0] NPC,
-    output reg  [31:0] PC
+    input clk,
+    input rst,
+    input stall,
+    input [2:0] NPCOP,
+    input [31:0] NPC,
+    output reg [31:0] PC
 );
 
     wire [31:0] PCPLUS4;
-    assign PCPLUS4 = PC + 4; // pc + 4
+    assign PCPLUS4 = PC + 4; // For normal instruction execution with no jump, we will put PC + 4
 
     always @(posedge clk, posedge rst)
         if (rst)
-            PC <= 32'h0000_0000;  //      PC <= 32'h0000_3000;
+            PC <= 32'h0000_0000; // Reset the PC
         else begin
             if (stall == 1) PC <= PC;
             else if (NPCOP != 3'b000) PC <= NPC;
